ebff54ebf2b0c8addad241c6237ec09495f9b9f8
Melih Koç <115876146+melihkocc@users.noreply.github.com>
2024-07-25 17:23:37 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='absolute w-screen flex flex-col justify-center items-center mb-2' style={{bottom : "0px"}}>
-      <Link className='underline text-blue-500' href='https://my-portfolio-sepia-nu-71.vercel.app/'>Portfolyo Sitesi</Link>
+      <Link className='underline text-blue-500' href='https://my-portfolio-eta-ashy.vercel.app/'>Portfolyo Sitesi</Link>
       <div className='mt-1'>© 2024 Melih Koç. Tüm hakları saklıdır.</div>
     </footer>
   )
